f35dac7c3eab078fdea802e7f46efdc20040f125
Ricky <rickdhanota@gmail.com>
2020-09-06 18:02:51 -0700

    adding all the values

--- twoNumberSum_EASY.py
+++ twoNumberSum_EASY.py
@@ -2,6 +2,6 @@ def twoNumberSum(array, targetSum):
     results = {}
     for i in range(len(array)):
         for j in range(len(array)):
-            
+            if array[i]+array[j] == targetSum
 
 print(twoNumberSum([ 3, 5, -4, 8, 11, 1, -1, 6], 10))
