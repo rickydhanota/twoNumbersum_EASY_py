154b452098e6d84f94d39685002e47b9ed0afaa8
Ricky <rickdhanota@gmail.com>
2020-09-06 17:58:59 -0700

    initial commit

---
+++ twoNumberSum_EASY.py
@@ -0,0 +1,7 @@
+def twoNumberSum(array, targetSum):
+    results = {}
+    for i in range(len(array)):
+        for j in range(len(array)):
+            
+
+print(twoNumberSum([ 3, 5, -4, 8, 11, 1, -1, 6], 10))
